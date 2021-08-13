daecb510528b3b140a0afc1dcc2548c013415d02
Evgeniia <evgarnaga@gmail.com>
2021-08-13 22:17:29 +0600

    feat: add a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result = n.toString();
+
+  if (result[0] === '-') {
+    result = result.slice(1);
+  }
+
+  result = Number(result.split('').reverse().join(''));
+
+  return result;
 }
